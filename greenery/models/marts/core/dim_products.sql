735f82f94a7b7ce742ab8b13dda5d43b068a1759
Peggy Fan <x_92@hotmail.com>
2023-04-24 23:02:20 +0000

    week2 work

---
+++ greenery/models/marts/core/dim_products.sql
@@ -0,0 +1,10 @@
+with products as (
+    select * from {{ ref('stg_postgres__products') }}
+)
+
+select
+product_id
+,product_name
+,price   -- Assumption is that price is fixed or based on current - need snapshot?
+-- ,inventory -- Refer to products_snapshot for inventory
+from products
