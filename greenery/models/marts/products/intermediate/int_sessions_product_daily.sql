82b9356ce14fe805285ed0e81a6bc448a9accee0
Peggy Fan <x_92@hotmail.com>
2023-04-27 23:31:19 +0000

    week3 proj

--- greenery/models/marts/products/intermediate/int_sessions_product_daily.sql
+++ greenery/models/marts/products/intermediate/int_sessions_product_daily.sql
@@ -11,24 +11,10 @@ with events as (
     from {{ ref ('stg_postgres__events') }}
 )
 , events1 as (
-    select
-        product_id
-        ,session_id
-        ,min(created_at)::date as activity_date
-        ,sum(case when event_type = 'add_to_cart' then 1 else 0 end) as add_to_carts_events
-        ,sum(case when event_type = 'checkout' then 1 else 0 end) as checkouts_events
-        ,sum(case when event_type = 'packaged_shipped' then 1 else 0 end) as package_shippeds_events
-        ,sum(case when event_type = 'page_view' then 1 else 0 end) as page_views_events
-    from events
-    group by 1,2
-)
-, events2 as (
-  select *
-  , BOOL_OR 
-
-
+   {{ session_event_count('event_type')}}
 
 )
+
 select {{ dbt_utils.generate_surrogate_key([
                 'activity_date', 
                 'product_id'
